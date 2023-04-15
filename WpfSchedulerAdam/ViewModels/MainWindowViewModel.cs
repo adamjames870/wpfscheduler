f0a205abfaadf8427d3cd6467252b32fea1fdbc6
Adam James <adam.james870@yahoo.co.uk>
2023-04-15 06:23:30 +0100

    Added time label panel

--- WpfSchedulerAdam/ViewModels/MainWindowViewModel.cs
+++ WpfSchedulerAdam/ViewModels/MainWindowViewModel.cs
@@ -12,10 +12,11 @@ namespace WpfSchedulerAdam.ViewModels;
 public class MainWindowViewModel : BaseViewModel
 {
     private readonly ActivityGridViewModel _activityGridViewModel;
+    private readonly TimeColumnViewModel _timeColumnViewModel;
 
     private int _datesToDisplay = 3;
     private int _locationsToDisplay = 4;
-    
+
     public MainWindowViewModel()
     {
         var dataFactory = new DataFactory();
@@ -43,9 +44,11 @@ public class MainWindowViewModel : BaseViewModel
         };
         
         _activityGridViewModel = new ActivityGridViewModel(gridDisplayConfiguration, activities);
+        _timeColumnViewModel = new TimeColumnViewModel(rowsDisplayConfiguration);
     }
     
     public ActivityGridViewModel ActivityGridViewModel => _activityGridViewModel;
+    public TimeColumnViewModel TimeColumnViewModel => _timeColumnViewModel;
     
     public int DatesToDisplay
     {
---
+++ WpfSchedulerAdam/ViewModels/TimeColumnViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using WpfSchedulerAdam.Models;
+
+namespace WpfSchedulerAdam.ViewModels;
+
+public class TimeColumnViewModel : BaseViewModel
+{
+    private ObservableCollection<TimeColInfo> _times;
+    private RowsDisplayConfiguration _rowsDisplayConfiguration;
+
+    public TimeColumnViewModel(RowsDisplayConfiguration rowsDisplayConfiguration)
+    {
+        _rowsDisplayConfiguration = rowsDisplayConfiguration;
+        var timeLabel = _rowsDisplayConfiguration.StartHour;
+        _times = new ObservableCollection<TimeColInfo>();
+        for (int i = 0; i < RowCount; i++)
+        {
+            var x = new TimeColInfo();
+            x.RowIndex = i;
+            if ( (i % _rowsDisplayConfiguration.RowsPerHour) == 0)
+            {
+                x.TimeLabel=$"{timeLabel:00}:00";
+                timeLabel++;
+            }
+            else
+            {
+                x.TimeLabel = " ";
+            }
+            _times.Add(x);
+        }
+    }
+    
+    public ObservableCollection<TimeColInfo> Times => _times;
+    public int RowCount => _rowsDisplayConfiguration.HoursToDisplay * _rowsDisplayConfiguration.RowsPerHour;
+
+    public class TimeColInfo
+    {
+        public int RowIndex { get; set; }
+        public string TimeLabel { get; set; }
+    }
+    
+}
+
