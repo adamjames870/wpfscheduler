43bec5a36ac87f90680a39b2d73c7c6de34e9b28
Adam James <adam.james870@yahoo.co.uk>
2023-04-15 07:17:42 +0100

    Tidying

---
+++ WpfScheduler.UI/Models/Internal/RangePanelModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WpfSchedulerAdam.Models.Internal;
+
+public class RangePanelModel
+{
+    public int SortIndex { get; set; }
+    public RowsDisplayConfiguration RowsDisplay { get; set; }
+    public DateOnly Date { get; set; }
+    public string Location { get; set; }
+    
+    public RangePanelModel()
+    {
+        
+    }
+}
